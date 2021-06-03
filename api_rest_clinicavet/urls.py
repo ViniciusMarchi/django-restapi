171b2120c5f283aef8d661e99c5e1a533fcc086e
Vinicius Marchi <vinicius.marchi@ufms.br>
2021-06-03 03:39:02 -0400

    atualizando urls

--- api_rest_clinicavet/urls.py
+++ api_rest_clinicavet/urls.py
@@ -3,4 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('pets.urls')),
+    path('', include('clientes.urls')),
+    path('', include('veterinarios.urls')),
+    path('', include('consultas.urls'))
 ]
